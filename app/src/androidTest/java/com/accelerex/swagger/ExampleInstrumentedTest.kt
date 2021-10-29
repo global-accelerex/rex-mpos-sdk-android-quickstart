903db5353f055bddb0aa2b134989f119ce6cc2b3
kareemadesola <kareemadesola1999@gmail.com>
2021-10-29 16:25:44 +0100

    refactor: rename package to com.accelerex.swagger

--- app/src/androidTest/java/com/accelerex/swagger/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/accelerex/swagger/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.accelerex.swagger
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/com/accelerex/swagger/ui/CategoryFragment.kt
+++ app/src/main/java/com/accelerex/swagger/ui/CategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui
+package com.accelerex.swagger.ui
 
 import android.os.Bundle
 import android.view.View
