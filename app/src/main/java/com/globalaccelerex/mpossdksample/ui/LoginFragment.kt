05124a991720845700fca6c2b60a9eea1381cfbc
kareemadesola <kareemadesola1999@gmail.com>
2021-11-08 08:38:54 +0100

    conditional navigation to take you to Login Screen

--- app/src/main/java/com/globalaccelerex/mpossdksample/ui/LoginFragment.kt
+++ app/src/main/java/com/globalaccelerex/mpossdksample/ui/LoginFragment.kt
@@ -3,8 +3,10 @@ package com.globalaccelerex.mpossdksample.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.globalaccelerex.mpossdksample.R
 import com.globalaccelerex.mpossdksample.databinding.FragmentLoginBinding
+import com.globalaccelerex.mpossdksample.ui.CatalogFragment.Companion.KEY_LOGIN
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -14,6 +16,16 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragmentLoginBinding = FragmentLoginBinding.bind(view)
+
+        val userLoggedInStateHandle = findNavController().previousBackStackEntry?.savedStateHandle
+        userLoggedInStateHandle?.set(KEY_LOGIN, false)
+
+        // Assign a click listener to take you to the catalog screen
+        fragmentLoginBinding!!.buttonSignIn.setOnClickListener {
+            // create an action from login screen to catalog screen
+            userLoggedInStateHandle?.set(KEY_LOGIN, true)
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
